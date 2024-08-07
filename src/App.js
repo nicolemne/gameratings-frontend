81133accaee1d34f3fa87fc7307670a51e875ff2
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-07 19:03:13 +0000

    deployement test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
-import Container from "react-bootstrap/Container";
-import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <div className="NavBar">
-        <NavBar />
-        <Container className={styles.Main}>
-          <Switch>
-            <Route exact path="/" render={() => <h1>Home Page</h1>} />
-            <Route exact path="/newpost" render={() => <h1>New Post</h1>} />
-            <Route
-              exact
-              path="/likedposts"
-              render={() => <h1>Liked Posts</h1>}
-            />
-            <Route exact path="/mygames" render={() => <h1>My Games</h1>} />
-            <Route exact path="/profile" render={() => <h1>Profile</h1>} />
-            <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
-            <Route exact path="/login" render={() => <h1>Log In</h1>} />
-            <Route exact path="/logout" render={() => <h1>Log Out</h1>} />
-            <Route render={() => <h1>404 page not found</h1>} />
-          </Switch>
-        </Container>
-      </div>
+    <div className="App">
+      <h1>Hello, World!</h1>
     </div>
   );
 }
