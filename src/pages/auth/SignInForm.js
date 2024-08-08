7b1d37df5c10cb818cf6f32042a3501c114fd383
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-08 03:27:08 +0000

    add handleChange and handleSubmit to SignInForm.js

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,14 +8,40 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
+import axios from "axios";
+
 function SignInForm() {
-  //   Add your component logic here
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
   return (
     <Row className={`${styles.Row} justify-content-center`}>
@@ -29,7 +55,7 @@ function SignInForm() {
         <Container className={`${appStyles.Content} p-4`}>
           <h1 className={styles.Header}>log in</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -37,8 +63,15 @@ function SignInForm() {
                 type="text"
                 placeholder="Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
@@ -47,8 +80,15 @@ function SignInForm() {
                 type="password"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
@@ -56,6 +96,12 @@ function SignInForm() {
             >
               Let's go!
             </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+            
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -14,6 +14,7 @@ import {
   Container,
   Alert,
 } from "react-bootstrap";
+
 import axios from "axios";
 
 const SignUpForm = () => {
