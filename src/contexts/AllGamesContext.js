93c77637dbe0e7101eb53789b7d457c5a570c5f6
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-11 03:11:21 +0000

    add AllGamesContext.js to fetch all games from api

---
+++ src/contexts/AllGamesContext.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+
+// Fetches all the games from the /games/ endpoint so all
+// games in the database can be displayed in a list or dropdown
+
+export const AllGamesContext = createContext();
+
+export const useAllGames = () => useContext(AllGamesContext);
+
+export const AllGamesProvider = ({ children }) => {
+  const [games, setGames] = useState([]);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosReq.get("/games/");
+      setGames(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  return (
+    <AllGamesContext.Provider value={games}>
+      {children}
+    </AllGamesContext.Provider>
+  );
+};
