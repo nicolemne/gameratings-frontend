9c6e2a285c50b5059391a84b2617c2f67702cf22
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-12 21:29:07 +0000

    update GameInfo to a more compact styling

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,34 +1,35 @@
 import React from "react";
-import styles from "../styles/SharedBoxStyles.module.css"
+import styles from "../styles/SharedBoxStyles.module.css";
 
 const GameInfo = ({ game }) => {
   return (
     <div className={styles.Box}>
       <h5 className="text-center">Game</h5>
-      <div className={styles.ImageBox}>
-        <img src={game?.image} alt="Game Cover" className={styles.Image} />
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Game:</strong> {game?.title || ""}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Platform</strong> {game?.platform?.name || ""}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Developer:</strong> {game?.game_developer || ""}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Genre:</strong> {game?.genre?.name || ""}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Release Year:</strong> {game?.release_year || ""}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Multiplayer:</strong> {game?.multiplayer ? "Yes" : "No"}
-      </div>
-      <div className={styles.InfoText}>
-        <strong>Average User Rating:</strong>{" "}
-        {game?.average_star_rating || "N/A"}
+      <div className={styles.InfoContainer}>
+        <div className={styles.InfoText}>
+          <strong>Game</strong> {game?.title || ""}
+        </div>
+        <div className={styles.InfoText}>
+          <strong>Platform</strong> {game?.platform?.name || ""}
+        </div>
+        <div className={styles.InfoText}>
+          <strong>Developer</strong> {game?.game_developer || ""}
+        </div>
+        <div className={styles.InfoText}>
+          <strong>Genre</strong> {game?.genre?.name || ""}
+        </div>
+        <div className={styles.InfoText}>
+          <strong>Release Year</strong> {game?.release_year || ""}
+        </div>
+        <div className={styles.InfoText}>
+          <strong>Multiplayer</strong> {game?.multiplayer ? "Yes" : "No"}
+        </div>
+        <div className={styles.ImageBox}>
+          <img src={game?.image} alt="Game Cover" className={styles.Image} />
+        </div>
+        <div className={styles.RatingBox}>
+          <strong>Rating</strong> {game?.average_star_rating || "N/A"}
+        </div>
       </div>
     </div>
   );
