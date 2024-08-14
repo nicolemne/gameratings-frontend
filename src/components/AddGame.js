77c0df32162874c51d244252d821572d50a46ba4
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-14 02:39:39 +0000

    add title, developer and year fields and handleChange function to AddGame modal

--- src/components/AddGame.js
+++ src/components/AddGame.js
@@ -1,25 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 
-function AddGameModal(props) {
+import btnStyles from "../styles/Button.module.css";
+
+function AddGameModal({ show, onHide }) {
+  const [gameData, setGameData] = useState({
+    title: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setGameData({
+      ...gameData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Modal
-      {...props}
+      show={show}
+      onHide={onHide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add a New Game
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">New Game</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>Placeholder text</p>
+        <Form>
+          <Form.Group controlId="formGameTitle">
+            <Form.Label>Game Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Game title"
+              name="title"
+              value={gameData.title}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.title?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group controlId="formGameDeveloper">
+            <Form.Label>Game Developer</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Developer"
+              name="developer"
+              value={gameData.developer}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.developer?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group controlId="formGameReleaseYear">
+            <Form.Label>Release Year</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Release Year"
+              name="release_year"
+              value={gameData.release_year}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.release_year?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button type="submit" className={btnStyles.AddGameBtn}>
+          Add Game
+        </Button>
       </Modal.Footer>
     </Modal>
   );
