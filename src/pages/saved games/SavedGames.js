fffc397c9a71cc176979fd85e85f9ad4b0ea0a91
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-17 05:28:46 +0000

    add game fields and styling to SavedGames

--- src/pages/saved games/SavedGames.js
+++ src/pages/saved games/SavedGames.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { axiosReq } from "../../api/axiosDefaults";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import styles from "../../styles/SavedGames.module.css";
 import Asset from "../../components/Asset";
+
+import { axiosReq } from "../../api/axiosDefaults";
 
 const SavedGames = () => {
   const [savedGames, setSavedGames] = useState([]);
@@ -28,20 +35,59 @@ const SavedGames = () => {
   }, []);
 
   return (
-    <div>
-      <h1>My Saved Games</h1>
-      {hasLoaded ? (
-        <div>
-          {savedGames.map((myGame) => (
-            <div key={myGame.id}>
-              <strong>{myGame.game_title}</strong> - {myGame.status}
+    <Container className={`${styles.Container}`}>
+      <Row className="justify-content-center">
+        <Col className="py-2 p-0 p-lg-2">
+          {hasLoaded ? (
+            <div>
+              {savedGames.map((myGame) => (
+                <div key={myGame.id} className={`p-3 mb-4 ${styles.GameCard}`}>
+                  <div className="text-center mb-4">
+                    <h5 className={`mb-2 ${styles.Title}`}>
+                      {myGame.game_title}
+                    </h5>
+                    <span>{myGame.status}</span>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-center">
+                    <div className={styles.ImageBox}>
+                      <img
+                        src={myGame?.game_image}
+                        alt="Game Cover"
+                        className={styles.Image}
+                      />
+                    </div>
+
+                    <div className="ms-3 text-start">
+                      <strong className={styles.GameInfo}>Developer</strong>
+                      <p className={styles.InfoText}>{myGame.game_developer}</p>
+                      <strong className={styles.GameInfo}>Release Year</strong>
+                      <p className={styles.InfoText}>
+                        {myGame.game_release_year}
+                      </p>
+                      <strong className={styles.GameInfo}>Genre</strong>
+                      <p className={styles.InfoText}>{myGame.game_genre}</p>
+                      <strong className={styles.GameInfo}>Platform</strong>
+                      <p className={styles.InfoText}>{myGame.game_platform}</p>
+                      <strong className={styles.GameInfo}>Multiplayer</strong>
+                      <p className={styles.InfoText}>
+                        {myGame.game_multiplayer ? "Yes" : "No"}
+                      </p>
+                      <strong className={styles.GameInfo}>Avg Rating</strong>
+                      <p className={styles.InfoText}>
+                        {myGame.game_average_star_rating}
+                      </p>
+                    </div>
+                  </div>
+                    {/* <hr className={styles.CustomHr}/> */}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <Asset spinner />
-      )}
-    </div>
+          ) : (
+            <Asset spinner />
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
